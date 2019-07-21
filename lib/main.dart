f8f6944c83b506e90b1fe655093012db1c639e2d
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-21 16:53:12 +0100

    Convert app stateless widget to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite color?",
+      "What's your favourite animal?",
+    ];
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("My First App"),
-        ),
-        body: Text("This is my default text!"),
+        home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("My First App"),
       ),
-    );
+      body: Column(
+        children: <Widget>[
+          Text(questions[questionIndex]),
+          RaisedButton(
+            child: Text("Answer 1"),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text("Answer 2"),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text("Answer 3"),
+            onPressed: () {
+              print('Answer 3 chosen');
+            },
+          ),
+        ],
+      ),
+    ));
+  }
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 }
